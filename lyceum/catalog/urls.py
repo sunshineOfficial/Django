bd323dda6ee4036eca850b20df88a02d7c8672d4
Dmitry Khrunin <thesunshineofficial@gmail.com>
2022-03-15 23:12:11 +0300

    minor fix

--- lyceum/catalog/urls.py
+++ lyceum/catalog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from catalog import views
 
 urlpatterns = [
-    path('<int:id>/', views.item_detail),
+    path('<int:pk>/', views.item_detail),
     path('', views.item_list)
 ]
--- lyceum/users/urls.py
+++ lyceum/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('users/<int:id>/', views.user_detail),
+    path('users/<int:pk>/', views.user_detail),
     path('users/', views.user_list),
     path('signup/', views.signup),
     path('profile/', views.profile)
