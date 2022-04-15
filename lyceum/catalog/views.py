98a63169434736df4e8d8a52016a410f1135d985
Dmitry Khrunin <thesunshineofficial@gmail.com>
2022-04-15 20:03:54 +0300

    Issue #7 additional fix + new design

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -24,31 +24,19 @@ def item_detail(request, pk):
     item = Item.objects.detailed_item(pk)
     stars = Rating.objects.filter(item=item, star__in=list(
         filter(lambda x: x != 0, map(lambda y: y[0], Rating.RATING_CHOICES)))).aggregate(Avg('star'), Count('star'))
-    try:
-        user = User.objects.get(username=request.user.username)
-        try:
-            user_star = Rating.objects.get(user_id=user.id, item=item)
-        except Rating.DoesNotExist:
-            user_star = Rating(user=user, item=item)
-        form = RatingUpdateForm(request.POST or None)
-        if form.is_valid():
-            user_star.star = form.cleaned_data['stars']
-            user_star.save()
-            return redirect('item_detail', pk)
-
-        context = {
-            'pk': pk,
-            'item': item,
-            'stars': stars,
-            'user': user,
-            'user_star': user_star,
-            'form': form
-        }
-    except User.DoesNotExist:
-        context = {
-            'pk': pk,
-            'item': item,
-            'stars': stars,
-            'user': None
-        }
+    user = User.objects.filter(username=request.user.username).first()
+    user_star = Rating.objects.get_or_create(user_id=user.id, item=item)[0]
+    form = RatingUpdateForm(request.POST or None)
+    if form.is_valid():
+        user_star.star = form.cleaned_data['stars']
+        user_star.save()
+        return redirect('item_detail', pk)
+    context = {
+        'pk': pk,
+        'item': item,
+        'stars': stars,
+        'user': user,
+        'user_star': user_star,
+        'form': form
+    }
     return render(request, template, context)
